60419bb7fd25f9c729bb9ecfb4e12b4e425a802d
Ryan Graham <ryanegraham1994@gmail.com>
2018-08-09 19:04:55 -0400

    created some basic interfaces

---
+++ BacklogTracker.Interfaces/BacklogTracker.Interfaces/IGameIdentity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BacklogTracker.Interfaces
+{
+    public interface IGameIdentity
+    {
+        string ProviderId { get; set; }
+        string Name { get; set; }
+    }
+}
